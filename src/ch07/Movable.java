6358eb996491ff6e00908fa5735ffd82c7ed8853
codingmatemoon <abc9529@naver.com>
2022-03-18 12:45:47 +0900

    intellJ Practice

---
+++ src/ch07/Movable.java
@@ -0,0 +1,5 @@
+package ch07;
+
+public interface Movable {
+    public void move();
+}
---
+++ src/ch08/Test.java
@@ -0,0 +1,18 @@
+package ch08;
+
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println();
+        Sample s = new Sample();
+        s.getTitle();
+        s.setTitle("s");
+        System.out.println("Last");
+        while (true) {
+
+        }
+//        List<String> list;
+    }
+}
