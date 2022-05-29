2f1ca0a5c1ca16dd07e28cf9f164cd50cd90b767
codingmatemoon <abc9529@naver.com>
2022-05-29 10:57:43 +0900

    합이 같은 부분집합(DFS 아마존 인터뷰) 최종

--- src/algorithm/dfs/합이 같은 부분집합(DFS 아마존 인터뷰).js
+++ src/algorithm/dfs/합이 같은 부분집합(DFS 아마존 인터뷰).js
@@ -20,18 +20,81 @@ N개의 원소로 구성된 자연수 집합이 주어지면, 이 집합을 두 
 YES
  */
 
-function solution(n){
+function solution(n, array){
 
-    let check = Array.from(n + 1, () => 0);
-    for (let i = 1; i<=n; i++) {
-        check[i] = 1;
-        console.log(check[i]);
+    let totalSum = array.reduce((a, b) => (a + b), 0);
+    // console.log("totalSum : " + totalSum);
+    let answer = "NO";
+    let flag = false;
+
+    function dfs(l, sum) {
+        if(flag)
+            return;
+        if (l > n - 1) {
+
+            let sameCheck = totalSum - sum;
+            if (sameCheck === sum) {
+                answer = "YES";
+                flag = true;
+                console.log("check : " + sum);
+            }
+            return;
+        }
+        dfs(l + 1, sum + array[l]);
+        dfs(l + 1, sum);
     }
-    console.log("test");
-    console.log(check);
+
+    dfs(0, 0);
+    return answer;
 }
 
-solution(2);
+console.log(solution(6, [1, 3, 5, 6, 7, 10]));
+
+/*
+dfs(0), dfs(1), dfs(2) 등을 호출하다 부분집합의 원소의 합이 같은 경우가 나오면 뒤에 부분집합 경우의 수가 더 있더라도 체크하지 않고 함수를 종료합니다. flag를 통해 이를 구현합니다.
+ */
+
+function solutionBefore(n, array){
+
+
+    // for (let i = 1; i<=n; i++) {
+    //     check[i] = 1;
+    //     console.log(check[i]);
+    // }
+    // console.log("test");
+    // console.log(check);
+    let check = Array.from(n + 1, () => 0);
+    let result = "NO";
+    function dfs(v){
+
+        if (v > n) {
+            let checkTrueSum = 0;
+            let checkFalseSum = 0;
+            for (let i = 1; i <= n; i++) {
+                if (check[i]) {
+                    checkTrueSum += parseInt(array[i - 1]);
+                } else {
+                    checkFalseSum += parseInt(array[i - 1]);
+                }
+            }
+            if(checkTrueSum === checkFalseSum)
+                result = "YES";
+            console.log(check);
+            console.log(checkTrueSum + " : " + checkFalseSum);
+            return;
+        }
+        check[v] = 1;
+        dfs(v + 1);
+        check[v] = 0;
+        dfs(v + 1);
+    }
+
+    dfs(1);
+    return result;
+}
+
+// console.log(solutionBefore(6, ['1', '3', '5', '6', '7','10']));
+// console.log(solutionBefore(6, ['1', '3', '5', '6', '7','10']));
 
 /*
 
@@ -44,4 +107,31 @@ solution(2);
 • 각 원소들의 포함여부를 체크할 배열 check를 만들고 관리합니다.
 • 방문할 경우 포함할 경우 check[i] = 1, 포함하지 않을 경우 0으로 값을 넣습니다. n개 예시로는 6개를 넘어서 방문하려할 경우 함수를 종료시킵니다. if(v == n+1) return;
 방문이 끝났을 때 check 배열의 값을 참조하여 각 부분집합 원소들의 합을 구하고 비교한 뒤 존재할 경우 YES를 출력하고 아닐 경우 NO를 출력합니다
+n개의 원소가 있다고 할 때 dfs(1)부터 시작할 경우 우선 1을 포함하는 경우부터 구합니다. check[1] = 1; dfs(n+1); check[1] = 0; dfs(n+1);
  */
+
+
+function solutionRef(arr){
+    let answer="NO", flag=0;
+    let total=arr.reduce((a, b)=>a+b, 0);
+    console.log(total);
+    let n=arr.length;
+    function DFS(L, sum){
+        if(flag) return;
+        if(L===n){
+            if((total-sum)===sum){
+                answer="YES";
+                flag=1;
+            }
+        }
+        else{
+            DFS(L+1, sum+arr[L]);
+            DFS(L+1, sum);
+        }
+    }
+    DFS(0, 0);
+    return answer;
+}
+
+let arr=[1, 3, 5, 6, 7, 10];
+// console.log(solutionRef(arr));
