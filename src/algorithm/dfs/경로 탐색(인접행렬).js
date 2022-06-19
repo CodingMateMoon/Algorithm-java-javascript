b33c9b45a421cb26e1717aad98418b90d6f1f61a
codingmatemoon <abc9529@naver.com>
2022-06-20 02:31:09 +0900

    경로 탐색(인접행렬) 참고

--- src/algorithm/dfs/경로 탐색(인접행렬).js
+++ src/algorithm/dfs/경로 탐색(인접행렬).js
@@ -65,13 +65,51 @@ function solution(n, m, array){
     }
 
     dfs(1, 1);
-    console.log("count : " + count);
+    // console.log("count : " + count);
     return answer;
 }
 
-console.log(solution(5, 9, [[1, 2], [1, 3], [1, 4], [2, 1], [2, 3], [2, 5], [3, 4], [4, 2], [4, 5]]));
+// console.log(solution(5, 9, [[1, 2], [1, 3], [1, 4], [2, 1], [2, 3], [2, 5], [3, 4], [4, 2], [4, 5]]));
 /*
 # 정점의 수 N=5, 간선의 수 M = 9가 주어지고 M줄에 걸쳐 간선 연결정보 1 2, 1 3, 1 4, 2 1 등이 주어집니다. 1번 정점에서 N = 5 정점으로 가는 총 가지수 6을 출력합니다. 1번 노드에서 시작해서 갈 수 있는 경우의 수는 2, 3, 4가 있습니다. 2를 선택한 경우 3, 5 가 있고 3을 선택한 경우 4가 있으며 4를 선택할 경우 5로 이동해서 도착합니다. 다시 2를 선택한 경우로 돌아가서 5로 이동하고 더이상 탐색할 게 없으므로 1번 노드로 돌아가서 3을 선택합니다. 이후 4를 선택하고 2, 5 중에서 2를 선택한 뒤 5로 이동하고 4로 돌아가서 5를 선택합니다. 행렬 이차원 배열 array[i][j]에서 i = 1일 때 갈 수 있는 경우는 2, 3, 4가 있습니다. 2를 선택한 경우 1은 체크로 제외하고 3 5에 갈 수 있습니다. 3을 선택한 경우 4, 5 가 있고 4를 선택한 경우 5가 있습니다.  다시 4->3->2 로 돌아가서 5를 선택합니다.
 dfs(level, currentIndex) = dfs(1, 1)
 정점 1에서 시작하므로 level = 1, currentIndex = 1에서 시작해서 for(let i = 2; i <= n; i++) 1행중에서 갈 수 있는 경우를 구합니다. 먼저 2번에 갈 수 있으므로 if(matrix[currentIndex][i]) check[i] = true; resultArray[level] = i; dfs(level +1, i); 2번을 체크하고 2행으로 갑니다. dfs(level + 1, currentIndex)
  */
+
+
+function solutionRef(n,arr) {
+
+    let answer = 0;
+    let graph = Array.from(Array(n+1), ()=> Array(n+1).fill(0));
+    let ch  = Array.from({length:n+1}, ()=>0);
+    path = [];
+    for (let [a,b] of arr) {
+        // console.log(a,b);
+        graph[a][b] = 1; // 무방향일경우 [b][a] = 1;
+    }
+    function dfs(v) {
+        if(v === n) {
+            answer++;
+            console.log(path);
+        }
+        else {
+            for(let i = 1; i <= n ; i++) {
+                if(graph[v][i] === 1 && ch[i] === 0) {
+                    ch[i] = 1;
+                    path.push(i);
+                    dfs(i);
+                    ch[i] = 0;
+                    path.pop();
+                }
+            }
+        }
+    }
+
+    path.push(1);
+    ch[1] = 1;
+    dfs(1);
+    return answer;
+}
+
+let arr = [[1, 2], [1, 3], [1, 4], [2, 1], [2, 3], [2, 5], [3, 4], [4, 2], [4, 5]];
+console.log(solutionRef(5,arr));
