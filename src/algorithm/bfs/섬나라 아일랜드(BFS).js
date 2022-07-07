7f654a2c358bd554843d2aaf5c3e6f1c1fe17505
codingmatemoon <abc9529@naver.com>
2022-07-08 07:14:44 +0900

    섬나라 아일랜드 BFS 참고

--- src/algorithm/bfs/섬나라 아일랜드(BFS).js
+++ src/algorithm/bfs/섬나라 아일랜드(BFS).js
@@ -52,7 +52,7 @@ function solution(n, graph){
         queue.push([x, y]);
         while (queue.length) {
             let position = queue.shift();
-            console.log(position);
+            // console.log(position);
             for (let i = 0; i < 8; i++) {
                 let nextX = position[0] + directionX[i];
                 let nextY = position[1] + directionY[i];
@@ -78,8 +78,49 @@ let graph =[ [1, 1, 0, 0, 0, 1, 0]
     ,[1, 0, 0, 0, 1, 0, 0]
     ,[1, 0, 1, 0, 1, 0, 0] ];
 
-console.log(solution(7, graph));
+// console.log(solution(7, graph));
 
 /*
 섬의 개수는 1로 표시된 값들이 연결된 그룹들을 구하는 것입니다. 상하좌우대각선으로 인접한 정점이 있는지 탐색하고 있으면 방문합니다. 방문한 정점을 체크하고 큐에 넣습니다. 주변에 더이상 방문할 정점이 없으면 종료합니다. for문을 통해 각 정점들에 대해 방문하지 않은 경우 count 개수를 증가시키고 check 또는 graph[x][y] = 0 으로 값을 변경하며 해당 위치를 방문합니다.
  */
+
+
+function solutionRef(board){
+    let answer=0;
+    let n=board.length;
+    let dx=[-1, -1, 0, 1, 1, 1, 0, -1];
+    let dy=[0, 1, 1, 1, 0, -1, -1, -1];
+    let queue=[];
+    for(let i=0; i<n; i++){
+        for(let j=0; j<n; j++){
+            if(board[i][j]===1){
+                board[i][j]=0;
+                queue.push([i, j]);
+                answer++;
+                while(queue.length){
+                    let [x,y]=queue.shift();
+                    for(let k=0; k<8; k++){
+                        let nx=x+dx[k];
+                        let ny=y+dy[k];
+                        if(nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny]===1){
+                            board[nx][ny]=0;
+                            queue.push([nx, ny]);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    return answer;
+}
+
+let arr=[[1, 1, 0, 0, 0, 1, 0],
+    [0, 1, 1, 0, 1, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0],
+    [1, 0, 1, 0, 1, 0, 0]];
+
+console.log(solutionRef(arr));
